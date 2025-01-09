05ebdf308955ff6f0bfadb6762eb222935d41cd8
Death <2907800178@qq.com>
2025-01-10 02:37:29 +0800

    添加金币指令

--- kubejs/server_scripts/util.js
+++ kubejs/server_scripts/util.js
@@ -23,3 +23,41 @@ PlayerCoin.prototype = {
 };
 
 global.PlayerCoin = PlayerCoin;
+
+ServerEvents.commandRegistry(event => {
+    const { commands: Commands, arguments: Arguments } = event;
+    event.register(
+        Commands.literal("coins")
+            .executes(ctx => {
+                let player = ctx.source.playerOrException;
+                let playerName = player.getName().getString();
+                let coin = new PlayerCoin(player).get();
+                player.tell(`${playerName} has ${coin} coins`);
+                return coin;
+            })
+            .then(
+                Commands.literal("add")
+                    .requires(src => src.hasPermission(Commands.LEVEL_ADMINS))
+                    .then(
+                        Commands.argument("count", Arguments.INTEGER.create(event)).executes(ctx => {
+                            let player = ctx.source.playerOrException;
+                            let coin = new PlayerCoin(player);
+                            coin.add(Arguments.INTEGER.getResult(ctx, "count"));
+                            return coin.get();
+                        })
+                    )
+            )
+            .then(
+                Commands.literal("set")
+                    .requires(src => src.hasPermission(Commands.LEVEL_ADMINS))
+                    .then(
+                        Commands.argument("count", Arguments.INTEGER.create(event)).executes(ctx => {
+                            let player = ctx.source.playerOrException;
+                            let coin = new PlayerCoin(player);
+                            coin.set(Arguments.INTEGER.getResult(ctx, "count"));
+                            return coin.get();
+                        })
+                    )
+            )
+    );
+});
