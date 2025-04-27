0cdf1a86789061a9a6057d79997b764bb3fafdb7
Death <2907800178@qq.com>
2025-04-27 22:42:46 +0800

    房间结束后,获取遗物奖励

---
+++ kubejs/server_scripts/cradRewards.js
@@ -0,0 +1,22 @@
+let relicCards = [];
+for (let relicName in global.relicMap) {
+    let cardId = RandomCardRewards.rl(relicName);
+    let card = new RCRCard(cardId, `item.kubejs.relic_${relicName}`, "descriptionKey", RandomCardRewards.rl("empty"), CardContent["item(java.lang.String)"](global.relicMap[relicName].id));
+    relicCards.push(card);
+
+}
+
+let relicRewardsPoolRl = RandomCardRewards.rl("relic_rewards");
+let relicRewardsPool = new CardPool(relicCards, relicRewardsPoolRl);
+CardPoolManager.addCardPool(relicRewardsPoolRl, relicRewardsPool);
+
+global.openCardRewardsScreen = function (player) {
+    NetworkHooks.openScreen(player, new RCRCardMenuProvider(relicRewardsPoolRl));
+};
+
+randomcardrewards.cardInvokePre(e => {
+    if(e.card.content().type() == "item"){
+        global.playerGetItem(e.player, Item.of(e.card.content().content()))
+        e.setCanceled(true);
+    }
+})
--- kubejs/startup_scripts/maze_generator.js
+++ kubejs/startup_scripts/maze_generator.js
@@ -1,23 +1,23 @@
-StartupEvents.registry("block", (event) => {
-	event
-		.create("data_block")
-		.textureAll("minecraft:block/iron_block")
-		.displayName("数据方块")
-		.unbreakable()
-		.blockEntity((entityInfo) => {}); //仅做数据保存 没有功能
+StartupEvents.registry("block", event => {
+    event
+        .create("data_block")
+        .textureAll("minecraft:block/iron_block")
+        .displayName("数据方块")
+        .unbreakable()
+        .blockEntity(entityInfo => {}); //仅做数据保存 没有功能
 
-	event
-		.create("room_core")
-		.textureAll("minecraft:block/diamond_block")
-		.displayName("房间核心")
-		.unbreakable()
-		.blockEntity((entityInfo) => {
-			entityInfo.serverTick(1, 0, (entity) => {
-				global.room_function(entity);
-			});
-		});
+    event
+        .create("room_core")
+        .textureAll("minecraft:block/diamond_block")
+        .displayName("房间核心")
+        .unbreakable()
+        .blockEntity(entityInfo => {
+            entityInfo.serverTick(1, 0, entity => {
+                global.room_function(entity);
+            });
+        });
 
-	/*
+    /*
     event.create("maze_generator")
     .textureAll("minecraft:block/diamond_block")
     .displayName("地图生成器")
@@ -35,8 +35,8 @@ StartupEvents.registry("block", (event) => {
  * @param {Internal.BlockEntity} entity
  */
 global.room_function = function (entity) {
-	let data = entity.persistentData;
-	let level = entity.getLevel();
+    let data = entity.persistentData;
+    let level = entity.getLevel();
 
     let mazeList = ["1a_entrance_1"];
     let mazeRandom = Math.floor(Math.random() * mazeList.length);
@@ -81,6 +81,7 @@ global.room_function = function (entity) {
             data.putInt("flag", flag + 1);
             let isspawnvalid = global.spawn_wave(level, entity, flag + 1);
             if (isspawnvalid == -1) {
+                giveReward(players);
                 global.build_room(level, rlist[stage], entity);
                 global.door_action(level, entity, true);
                 // level.server.scheduleInTicks(1, () => {level.getBlock(entity.blockPos).set('air')})
@@ -94,6 +95,13 @@ global.room_function = function (entity) {
     let entitylist = level.getEntitiesWithin(rng);
     */
 };
+
+function giveReward(players) {
+    for (const player of players) {
+		global.openCardRewardsScreen(player)
+    }
+}
+
 /**
  * 构建房间
  * @param {Internal.LevelKJS} level
@@ -101,18 +109,18 @@ global.room_function = function (entity) {
  * @param {Internal.BlockEntity} entity
  */
 global.build_room = function (level, name, entity) {
-	let data = entity.persistentData;
-	//将旧房间核心置为失效
-	data.putInt("disabled", 1);
-	let rlist = data.room_list;
-	let stage = data.getInt("stage") + 1;
-	console.log(name);
-	let roomdata = JsonIO.read(`./dungeon_json/${name}.json`) || {};
-	let pos = entity.getBlockPos();
-	// let target_pos = [pos.getX() + pos_shift[0], pos.getY(), pos.getZ() - roomdata.chunk_size.z * 16 + pos_shift[1]];
-	//新房间核心位置
-	let target_pos = pos.offset(roomdata.block_shift.x - data.getInt("dx"), 0, roomdata.block_shift.z - roomdata.chunk_size.z * 16);
-	/* 
+    let data = entity.persistentData;
+    //将旧房间核心置为失效
+    data.putInt("disabled", 1);
+    let rlist = data.room_list;
+    let stage = data.getInt("stage") + 1;
+    console.log(name);
+    let roomdata = JsonIO.read(`./dungeon_json/${name}.json`) || {};
+    let pos = entity.getBlockPos();
+    // let target_pos = [pos.getX() + pos_shift[0], pos.getY(), pos.getZ() - roomdata.chunk_size.z * 16 + pos_shift[1]];
+    //新房间核心位置
+    let target_pos = pos.offset(roomdata.block_shift.x - data.getInt("dx"), 0, roomdata.block_shift.z - roomdata.chunk_size.z * 16);
+    /* 
     let strblock = level.getBlock(target_pos[0], target_pos[1] - 1, target_pos[2]);
     strblock.set("minecraft:structure_block");
     strblock.mergeEntityData({ name: `kubejs:${name}`, posY: 2 });
@@ -125,51 +133,51 @@ global.build_room = function (level, name, entity) {
     new_core.set("minecraft:air");
     new_core.set("kubejs:room_core");
 
-	let new_data = new_core.entity.persistentData;
-	new_data.putString("roomtype", roomdata.roomtype);
-	let range = [];
-	for (let i = 0; i < 3; i++) {
-		range.push(parseInt(roomdata.detect_range.from[i]));
-		range.push(parseInt(roomdata.detect_range.to[i]));
-	}
-	console.log(range.join());
-	new_data.detect_range = range;
-	new_data.putInt("stage", stage);
-	new_data.putString("name", name);
-	new_data.putInt("dx", roomdata.block_shift.x);
+    let new_data = new_core.entity.persistentData;
+    new_data.putString("roomtype", roomdata.roomtype);
+    let range = [];
+    for (let i = 0; i < 3; i++) {
+        range.push(parseInt(roomdata.detect_range.from[i]));
+        range.push(parseInt(roomdata.detect_range.to[i]));
+    }
+    console.log(range.join());
+    new_data.detect_range = range;
+    new_data.putInt("stage", stage);
+    new_data.putString("name", name);
+    new_data.putInt("dx", roomdata.block_shift.x);
 
-	//如果是入口房间, 则生成房间列表
-	if (roomdata.roomtype == "entrance") {
-		let enemy_rooms = roomdata.theme_data.enemy_rooms;
-		let room_list = [];
+    //如果是入口房间, 则生成房间列表
+    if (roomdata.roomtype == "entrance") {
+        let enemy_rooms = roomdata.theme_data.enemy_rooms;
+        let room_list = [];
 
-		let id_list = [];
-		let weight_b = [];
-		let weight = 0;
-		for (const enemy_room of enemy_rooms) {
-			weight += enemy_room.weight;
-			weight_b.push(weight);
-			id_list.push(enemy_room.id);
-		}
+        let id_list = [];
+        let weight_b = [];
+        let weight = 0;
+        for (const enemy_room of enemy_rooms) {
+            weight += enemy_room.weight;
+            weight_b.push(weight);
+            id_list.push(enemy_room.id);
+        }
 
-		room_list.push(name);
+        room_list.push(name);
 
-		for (let i = 0; i < 6; i++) {
-			// 先固定生成六个试一试咸淡
-			let rnd = Math.random() * weight;
-			for (let j = 0; j < weight_b.length; j++) {
-				if (rnd <= weight_b[j]) {
-					room_list.push(id_list[j]);
-					break;
-				}
-			}
-		}
+        for (let i = 0; i < 6; i++) {
+            // 先固定生成六个试一试咸淡
+            let rnd = Math.random() * weight;
+            for (let j = 0; j < weight_b.length; j++) {
+                if (rnd <= weight_b[j]) {
+                    room_list.push(id_list[j]);
+                    break;
+                }
+            }
+        }
 
-		room_list.push(roomdata.theme_data.end_room);
-		new_data.room_list = room_list;
-	} else {
-		new_data.room_list = rlist;
-	}
+        room_list.push(roomdata.theme_data.end_room);
+        new_data.room_list = room_list;
+    } else {
+        new_data.room_list = rlist;
+    }
 };
 /**
  * 开关门
@@ -178,37 +186,37 @@ global.build_room = function (level, name, entity) {
  * @param {Boolean} isopen
  */
 global.door_action = function (level, entity, isopen) {
-	let data = entity.persistentData;
-	let name = data.getString("name");
-	let roomdata = JsonIO.read(`./dungeon_json/${name}.json`) || {};
-	let doordata = roomdata.door_data;
-	let pos = entity.getBlockPos();
-	// console.log(doordata);
-	for (const door of doordata) {
-		let targetpos = pos.offset(door.pos[0], door.pos[1] + 1, door.pos[2]);
-		level.spawnParticles(
-			"minecraft:campfire_signal_smoke",
-			false,
-			targetpos.x + 0.5,
-			targetpos.y + door.height / 2,
-			targetpos.z + 0.5,
-			0.5,
-			door.height / 2,
-			0.5,
-			4 * door.height,
-			0.01
-		);
-		for (let j = 0; j < door.height; j++) {
-			targetpos = targetpos.above();
-			let targetblock = level.getBlock(targetpos);
-			// console.log(targetblock.pos.toString());
-			if (!isopen) {
-				targetblock.set(door.block);
-			} else {
-				targetblock.set("minecraft:air");
-			}
-		}
-	}
+    let data = entity.persistentData;
+    let name = data.getString("name");
+    let roomdata = JsonIO.read(`./dungeon_json/${name}.json`) || {};
+    let doordata = roomdata.door_data;
+    let pos = entity.getBlockPos();
+    // console.log(doordata);
+    for (const door of doordata) {
+        let targetpos = pos.offset(door.pos[0], door.pos[1] + 1, door.pos[2]);
+        level.spawnParticles(
+            "minecraft:campfire_signal_smoke",
+            false,
+            targetpos.x + 0.5,
+            targetpos.y + door.height / 2,
+            targetpos.z + 0.5,
+            0.5,
+            door.height / 2,
+            0.5,
+            4 * door.height,
+            0.01
+        );
+        for (let j = 0; j < door.height; j++) {
+            targetpos = targetpos.above();
+            let targetblock = level.getBlock(targetpos);
+            // console.log(targetblock.pos.toString());
+            if (!isopen) {
+                targetblock.set(door.block);
+            } else {
+                targetblock.set("minecraft:air");
+            }
+        }
+    }
 };
 
 /**
@@ -218,18 +226,18 @@ global.door_action = function (level, entity, isopen) {
  * @param {Integer} wave
  */
 global.spawn_wave = function (level, entity, wave) {
-	let data = entity.persistentData;
-	let name = data.getString("name");
-	let roomdata = JsonIO.read(`./dungeon_json/${name}.json`) || {};
-	if (wave > roomdata.waves) return -1;
-	let wavedata = roomdata.wave_data[wave - 1];
-	let pos = entity.getBlockPos();
-	for (const wave of wavedata) {
-		let targetpos = pos.offset(wave.pos[0], wave.pos[1], wave.pos[2]);
-		level.spawnParticles("minecraft:poof", false, targetpos.x + 0.5, targetpos.y + 1, targetpos.z + 0.5, 0.5, 1, 0.5, 10, 0.01);
-		let target = level.createEntity(wave.type);
-		target.setPosition(targetpos.x + 0.5, targetpos.y + 0.1, targetpos.z + 0.5);
-		target.spawn();
-	}
-	return 1;
+    let data = entity.persistentData;
+    let name = data.getString("name");
+    let roomdata = JsonIO.read(`./dungeon_json/${name}.json`) || {};
+    if (wave > roomdata.waves) return -1;
+    let wavedata = roomdata.wave_data[wave - 1];
+    let pos = entity.getBlockPos();
+    for (const wave of wavedata) {
+        let targetpos = pos.offset(wave.pos[0], wave.pos[1], wave.pos[2]);
+        level.spawnParticles("minecraft:poof", false, targetpos.x + 0.5, targetpos.y + 1, targetpos.z + 0.5, 0.5, 1, 0.5, 10, 0.01);
+        let target = level.createEntity(wave.type);
+        target.setPosition(targetpos.x + 0.5, targetpos.y + 0.1, targetpos.z + 0.5);
+        target.spawn();
+    }
+    return 1;
 };
